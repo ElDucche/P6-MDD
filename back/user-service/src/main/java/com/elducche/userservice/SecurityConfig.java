a5b7f62da6b7772a98f84ef7ec6aaa65126d9834
Guillaume LEDUC <82090419+ElDucche@users.noreply.github.com>
2025-07-11 10:49:22 +0000

    Fin de configuration du backend

--- back/user-service/src/main/java/com/elducche/userservice/SecurityConfig.java
+++ back/user-service/src/main/java/com/elducche/userservice/SecurityConfig.java
@@ -2,21 +2,21 @@ package com.elducche.userservice;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-            .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
-                .anyRequest().authenticated()
-            );
-        return http.build();
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .authorizeExchange(exchanges -> exchanges
+                .pathMatchers("/api/auth/register", "/api/auth/login").permitAll()
+                .anyExchange().authenticated()
+            )
+            .build();
     }
 }
